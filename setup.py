4e46951f425273885bbe4f1f8c24196b3a4ddd6e
JustMaffie <jorivanee@hotmail.com>
2017-06-23 15:27:01 +0200

    Update I guess

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='DBans',
-      version='1.7',
+      version='1.8',
       description='Discord Bans Python',
       long_description=long_description,
       url='https://github.com/JustMaffie/dbanspy',
