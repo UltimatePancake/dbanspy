09d1589beca6b1279a3713542c8e79dc53245f72
UltimatePancake <pier.gaetani@gmail.com>
2017-08-19 01:31:39 -0600

    async dbans

--- dbans/dbans.py
+++ dbans/dbans.py
@@ -1,33 +1,33 @@
-import requests
+import aiohttp
 
-class DBans(object):
 
+class DBans:
     def __init__(self, **kwargs):
         if "token" not in kwargs:
             raise RuntimeError("Token not provided")
-
         self.token = kwargs['token']
 
-    def lookup(self, **kwargs):
-        if "id" not in kwargs:
+    async def lookup(self, **kwargs):
+        if "user_id" not in kwargs:
             raise RuntimeError("User ID not provided")
-        token = self.token
+
         payload = {
-            "token": token,
-            "userid": kwargs['id']
+            "token": self.token,
+            "userid": kwargs['user_id']
         }
-        resp = requests.post("https://bans.discordlist.net/api", data=payload)
-        final = resp.text
-        resp.close()
+
+        async with aiohttp.ClientSession() as session:
+            async with session.post('https://bans.discordlist.net/api', data=payload) as resp:
+                final = await resp.text()
+
         if "No token specified!" in final:
-        	raise RuntimeError("Token is not provided, please create a new instance or use the Update method")
+            raise RuntimeError("Token is not provided, please create a new instance or use the Update method")
         elif "Invalid token." in final:
-        	raise LookupError("The provided token is invalid, please create a new instance or use the Update method")
+            raise LookupError("The provided token is invalid, please create a new instance or use the Update method")
         else:
-        	return final.lower() in ("true")
+            return final.lower() in ("true")
 
-
-    def update(self, **kwargs):
-        if "token" not in kwargs:
+    def update(self, token):
+        if not token:
             raise RuntimeError("Token not provided")
-        self.token = kwargs['token']
+        self.token = token
